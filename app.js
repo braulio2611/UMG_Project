0afeb45147ecb864ee43620899f74c8819ba1853
braulio2611 <81401319+braulio2611@users.noreply.github.com>
2021-03-26 22:01:29 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const texto1=document.getElementById("texto1")
+const texto2=document.getElementById("texto2")
+const texto3=document.getElementById("texto3")
+const texto4=document.getElementById("texto4")
+const texto5=document.getElementById("texto5")
+const img=document.getElementById("DANY2")
+
+function datos1(){
+texto1.innerHTML="Braulio García"
+texto2.innerHTML="Edad: 19"
+texto3.innerHTML="Genero: Masculino"
+texto4.innerHTML="No. Carnét: 9989-21-4221"
+texto5.innerHTML=" "
+img.src="img1.jpg"
+}
+
+function datos2(){
+    texto1.innerHTML="Instituto Evangélico America Latina"
+    texto2.innerHTML="Zona 13"
+    texto3.innerHTML="Fundador: Don Virilio y Donña Beatriz"
+    texto4.innerHTML="Fundado en 1954"
+    texto5.innerHTML=" "
+    }
+
+    function datos3(){
+        texto1.innerHTML="Metodología: 100pts"
+        texto2.innerHTML="Lógica: 100pts"
+        texto3.innerHTML="Introducción: 100pts"
+        texto4.innerHTML="Contabilidad 1: 100pts"
+        texto5.innerHTML="Desarrollo humano: 100pts"
+        }
+        
+        function datos4(){
+            texto1.innerHTML="Tocar bateria"
+            texto2.innerHTML="Programar"
+            texto3.innerHTML="Escuchar música"
+            texto4.innerHTML="Jugar básquetbol"
+            texto5.innerHTML=" "
+            }
+
